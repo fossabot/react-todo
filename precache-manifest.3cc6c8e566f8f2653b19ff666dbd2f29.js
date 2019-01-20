4157809f3a309f89b4896c96ca0d8cc3c59afb40
Ruben Mateus <ruben.mateus3@gmail.com>
2019-01-20 01:25:23 +0000

    Updates

---
+++ precache-manifest.3cc6c8e566f8f2653b19ff666dbd2f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "7ab2a71d42636304e9e8",
+    "url": "/react-todo/static/js/main.7ab2a71d.chunk.js"
+  },
+  {
+    "revision": "f5eb11596f52b644155e",
+    "url": "/react-todo/static/js/1.f5eb1159.chunk.js"
+  },
+  {
+    "revision": "7ab2a71d42636304e9e8",
+    "url": "/react-todo/static/css/main.7a1ed3c3.chunk.css"
+  },
+  {
+    "revision": "37ee4138ae112dc3e1fdab75371e347e",
+    "url": "/react-todo/index.html"
+  }
+];
